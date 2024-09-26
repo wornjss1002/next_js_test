19678e070b596ba0fe0476a82316d96403d80e59
Jae Gweon Cheon <wornjss102@naver.com>
2024-09-26 17:08:23 +0900

    1st

---
+++ app/posts/new/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NewPostPage() {
+  return (
+  <div><h1>NewPostPage</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam labore ipsam, porro deleniti assumenda facere explicabo reprehenderit fugit iusto molestiae dolores nulla? Ex unde laudantium quod amet? Cumque, repudiandae veritatis?</p><div>hello</div></div>
+    
+)
+  
+}
---
+++ app/posts/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PostsPage() {
+  return (
+    <div className='text-center'>
+      <h1>welcome!</h1>
+      
+    </div>
+  )
+}
